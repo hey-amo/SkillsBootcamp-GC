96047de7ee470a1a035e736512f64200eaae8c22
AD <silentflute.ad@gmail.com>
2025-05-06 11:49:35 +0100

    First day, going over basics of variables, console logs.

---
+++ Week01/Day01.js
@@ -0,0 +1,30 @@
+// Day 01 JS
+// :Week 01 - Recapping on javascript
+
+// :Day 01 Skills Bootcamp
+
+// Variables
+let age = 35;
+let height = 120;
+let total = age + height;
+
+height += 20;
+
+let message = "Hello" + " world";
+
+console.log("Height = " + height);
+console.log(total);
+console.log(message);
+
+// --
+
+if (age > 18) {
+    console.log("That's ok");
+} else {
+    console.log("Not allowed");
+}
+
+console.log("Age:"  + age);
+
+// --
+
