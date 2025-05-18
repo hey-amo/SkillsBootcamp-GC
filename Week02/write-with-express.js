6f7db0b58dc21f872dd50d000ed8919ce6bea6bf
AD <silentflute.ad@gmail.com>
2025-05-18 11:07:48 +0100

    Adding schedule.  Updating express file to write and append a new line

--- Week02/write-with-express.js
+++ Week02/write-with-express.js
@@ -5,17 +5,32 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-let data = "Learning how to write to a file with node/express\n";
-
-// GET: /write-file/ route 
-app.get("/write-file", (req, res) => {
-	    
-    // Write data to a file
+// Simple route to write initial data (overwrite)
+app.get('/write-file', (req, res) => {
+    const data = "Learning how to write to a file with Node/Express\n";
     fs.writeFile('output.txt', data, (err) => {
-        if (err) throw err;
+        if (err) {
+            console.error("Write error:", err);
+            return res.status(500).send("Error writing file");
+        }
+        res.send("Initial data written");
     });
+});
 
-    res.send("\nCompleted write");
+// Append a single new line to the file
+app.post('/append-new-line', (req, res) => {
+    const line = req.body.line || 'New line\n';
+    fs.appendFile('output.txt', line + '\n', (err) => {
+        if (err) {
+            console.error("Append line error:", err);
+            return res.status(500).send("Error appending line");
+        }
+        res.send("Line appended");
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`);
 });
 
 
