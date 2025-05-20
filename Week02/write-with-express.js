6093d0b0afa76aa92600210a4364e589970d2564
AD <silentflute.ad@gmail.com>
2025-05-20 09:53:31 +0100

    update the write with express file

--- Week02/write-with-express.js
+++ Week02/write-with-express.js
@@ -5,6 +5,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Parse JSON requests
+app.use(express.json());
+
 // Simple route to write initial data (overwrite)
 app.get('/write-file', (req, res) => {
     const data = "Learning how to write to a file with Node/Express\n";
@@ -13,25 +16,24 @@ app.get('/write-file', (req, res) => {
             console.error("Write error:", err);
             return res.status(500).send("Error writing file");
         }
-        res.send("Initial data written");
+        res.send("Wrote to file");
     });
 });
 
 // Append a single new line to the file
-app.post('/append-new-line', (req, res) => {
-    const line = req.body.line || 'New line\n';
+app.get('/append', (req, res) => {
+    const line = req.body.line || 'New line';
     fs.appendFile('output.txt', line + '\n', (err) => {
         if (err) {
             console.error("Append line error:", err);
             return res.status(500).send("Error appending line");
         }
-        res.send("Line appended");
+        res.send("Appended to file");
     });
 });
 
 app.listen(port, () => {
     console.log(`App running on http://localhost:${port}`);
 });
-
 
 console.log("Finished app");
