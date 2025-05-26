445e40a832aa0091f510be9c0d223444514a4505
AD <silentflute.ad@gmail.com>
2025-05-26 19:51:29 +0100

    remove unnecessary code

--- assignment/assignment02/app.js
+++ assignment/assignment02/app.js
@@ -30,38 +30,6 @@ app.get('/', (req, res) => {
     res.send('Simple Movie API is running');
 });
 
-// # POST - Create 1 movie
-// app.post('/movies/', (req, res) => {
-//     const movie = req.body;
-
-//     if (!movie.title) {
-//         return req
-//     }
-
-//     db.collection('movies').insertOne(movie, (err, result) => {
-//         if (err) {
-//             console.error("Error adding movie - " err);
-//             return res.status(500).send("error adding movie");
-//         }
-
-//         res.status(201).send("Movie added");
-//     });
-// });
-
-// // # POST - Update single movie, with given ID
-// app.post('movies/update/:id', (req, res) => {
-
-// });
-
-// // # GET - Get all movies, limit 20
-// app.get('/movies/', (req, res) => {
-
-// });
-
-// // # GET - Single movie using ID
-// app.get('/movies/movie/:id', (req, res) => {
-
-// });
 
 // # App running
 app.listen(port, () => {
