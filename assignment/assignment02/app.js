a6dc9f708386234a4a93949fa1313b4acfe32f44
AD <silentflute.ad@gmail.com>
2025-06-24 10:10:17 +0100

    updating app.js

--- assignment/assignment02/app.js
+++ assignment/assignment02/app.js
@@ -25,10 +25,56 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
     console.log('Connected to MongoDB');
 });
 
-// Simple route to test server
+
+// Movie Record
+const movie = {
+    title: "Batman Begins",
+    year: 2005,
+    type: "Movie",
+    seasons: null,
+    episodes: null,
+    director: "Christopher Nolan",
+    imdbRating: "8.2",
+    poster: "https://m.media-amazon.com/images/M/MV5BODIyMDdhNTgtNDlmOC00MjUxLWE2NDItODA5MTdkNzY3ZTdhXkEyXkFqcGc@._V1_SX300.jpg",
+    cast: [
+        "Christian Bale",
+        "Michael Caine",
+        "Liam Neeson",
+        "Katie Holmes",
+        "Gary Oldman"
+    ]
+};
+
+
+
+// # GET - Read:: List all movies
 app.get('/', (req, res) => {
     res.send('Simple Movie API is running');
+
+    // List all movies from the movie db
 });
+
+// # PUT - Create:: movie
+app.post('/insert-movie/', (req, res) => {
+    // Insert a movie
+});
+
+// # GET - Read:: Single a movie
+app.post('/read-movie/{id}', (req, res) => {
+    // Find a movie that matches the ID, if any
+});
+
+
+// # PUT - Update:: Update a single movie
+app.post('/update-movie/{id}', (req, res) => {
+    // Find a movie that matches the ID, if any
+});
+
+// # GET - Delete:: Delete a single movie 
+app.get('/delete-movie/{id}', (req, res) => {
+    // Find a movie that matches the ID, then delete it, if it exists
+});
+
 
 
 // # App running
